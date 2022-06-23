65b40983d65c5d5b8b2fe1408604749875adb263
Jheflin1995 <106621400+Jheflin1995@users.noreply.github.com>
2022-06-22 22:38:48 -0500

    updated tables

--- src/main/java/com/revature/models/RequestStatus.java
+++ src/main/java/com/revature/models/RequestStatus.java
@@ -1,5 +1,7 @@
 package com.revature.models;
 
+import java.util.Objects;
+
 import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
@@ -18,4 +20,54 @@ public class RequestStatus {
 	
 	@Column
 	private String status;
+
+	public RequestStatus() {
+		super();
+	}
+
+	public RequestStatus(int id, String status) {
+		super();
+		this.id = id;
+		this.status = status;
+	}
+
+	public int getId() {
+		return id;
+	}
+
+	public void setId(int id) {
+		this.id = id;
+	}
+
+	public String getStatus() {
+		return status;
+	}
+
+	public void setStatus(String status) {
+		this.status = status;
+	}
+
+	@Override
+	public int hashCode() {
+		return Objects.hash(id, status);
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		RequestStatus other = (RequestStatus) obj;
+		return id == other.id && Objects.equals(status, other.status);
+	}
+
+	@Override
+	public String toString() {
+		return "RequestStatus [id=" + id + ", status=" + status + "]";
+	}
+	
+	
 }
--- src/main/java/com/revature/models/Requests.java
+++ src/main/java/com/revature/models/Requests.java
@@ -1,5 +1,8 @@
 package com.revature.models;
 
+import java.sql.Date;
+import java.util.Objects;
+
 import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
@@ -45,6 +48,139 @@ public class Requests {
 	@ManyToOne
 	@JoinColumn(name="rem_type")
 	private RemType type;
+
+	public Requests() {
+		super();
+	}
+
+	public Requests(double amount, Date rEIMB_SUBMITTED, Date rEIM_RESOLVED, String description, Employee rEIMB_AUTHOR,
+			Employee rEIMB_RESOLVER, RequestStatus status, RemType type) {
+		super();
+		this.amount = amount;
+		REIMB_SUBMITTED = rEIMB_SUBMITTED;
+		REIM_RESOLVED = rEIM_RESOLVED;
+		this.description = description;
+		REIMB_AUTHOR = rEIMB_AUTHOR;
+		REIMB_RESOLVER = rEIMB_RESOLVER;
+		this.status = status;
+		this.type = type;
+	}
+
+	public Requests(int id, double amount, Date rEIMB_SUBMITTED, Date rEIM_RESOLVED, String description,
+			Employee rEIMB_AUTHOR, Employee rEIMB_RESOLVER, RequestStatus status, RemType type) {
+		super();
+		this.id = id;
+		this.amount = amount;
+		REIMB_SUBMITTED = rEIMB_SUBMITTED;
+		REIM_RESOLVED = rEIM_RESOLVED;
+		this.description = description;
+		REIMB_AUTHOR = rEIMB_AUTHOR;
+		REIMB_RESOLVER = rEIMB_RESOLVER;
+		this.status = status;
+		this.type = type;
+	}
+
+	public int getId() {
+		return id;
+	}
+
+	public void setId(int id) {
+		this.id = id;
+	}
+
+	public double getAmount() {
+		return amount;
+	}
+
+	public void setAmount(double amount) {
+		this.amount = amount;
+	}
+
+	public java.sql.Date getREIMB_SUBMITTED() {
+		return REIMB_SUBMITTED;
+	}
+
+	public void setREIMB_SUBMITTED(java.sql.Date rEIMB_SUBMITTED) {
+		REIMB_SUBMITTED = rEIMB_SUBMITTED;
+	}
+
+	public java.sql.Date getREIM_RESOLVED() {
+		return REIM_RESOLVED;
+	}
+
+	public void setREIM_RESOLVED(java.sql.Date rEIM_RESOLVED) {
+		REIM_RESOLVED = rEIM_RESOLVED;
+	}
+
+	public String getDescription() {
+		return description;
+	}
+
+	public void setDescription(String description) {
+		this.description = description;
+	}
+
+	public Employee getREIMB_AUTHOR() {
+		return REIMB_AUTHOR;
+	}
+
+	public void setREIMB_AUTHOR(Employee rEIMB_AUTHOR) {
+		REIMB_AUTHOR = rEIMB_AUTHOR;
+	}
+
+	public Employee getREIMB_RESOLVER() {
+		return REIMB_RESOLVER;
+	}
+
+	public void setREIMB_RESOLVER(Employee rEIMB_RESOLVER) {
+		REIMB_RESOLVER = rEIMB_RESOLVER;
+	}
+
+	public RequestStatus getStatus() {
+		return status;
+	}
+
+	public void setStatus(RequestStatus status) {
+		this.status = status;
+	}
+
+	public RemType getType() {
+		return type;
+	}
+
+	public void setType(RemType type) {
+		this.type = type;
+	}
+
+	@Override
+	public int hashCode() {
+		return Objects.hash(REIMB_AUTHOR, REIMB_RESOLVER, REIMB_SUBMITTED, REIM_RESOLVED, amount, description, id,
+				status, type);
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		Requests other = (Requests) obj;
+		return Objects.equals(REIMB_AUTHOR, other.REIMB_AUTHOR) && Objects.equals(REIMB_RESOLVER, other.REIMB_RESOLVER)
+				&& Objects.equals(REIMB_SUBMITTED, other.REIMB_SUBMITTED)
+				&& Objects.equals(REIM_RESOLVED, other.REIM_RESOLVED)
+				&& Double.doubleToLongBits(amount) == Double.doubleToLongBits(other.amount)
+				&& Objects.equals(description, other.description) && id == other.id
+				&& Objects.equals(status, other.status) && Objects.equals(type, other.type);
+	}
+
+	@Override
+	public String toString() {
+		return "Requests [id=" + id + ", amount=" + amount + ", REIMB_SUBMITTED=" + REIMB_SUBMITTED + ", REIM_RESOLVED="
+				+ REIM_RESOLVED + ", description=" + description + ", REIMB_AUTHOR=" + REIMB_AUTHOR
+				+ ", REIMB_RESOLVER=" + REIMB_RESOLVER + ", status=" + status + ", type=" + type + "]";
+	}
 	
 	
 	
