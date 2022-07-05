0ea9b98f31bff2fccdb85f51b2d0c7d7fc4a70e0
Jheflin1995 <106621400+Jheflin1995@users.noreply.github.com>
2022-07-04 23:11:34 -0500

    Update employee-table-scripts.js

--- src/main/webapp/assets/scripts/employee-table-scripts.js
+++ src/main/webapp/assets/scripts/employee-table-scripts.js
@@ -105,7 +105,7 @@ function fetchEmps() {
 
     // the following request will work for testing on localhost
     
-    fetch(`http://localhost:8080/employee-servlet-app/employees`)
+    fetch(`http://${hostname}/employee-servlet-app/employees`)
     // this is changed because the port will be inferred when deployed on Elastic beanstalk
     .then(response => response.json()) // trakes a json string and transforms
                                         // it to a javaScript object
